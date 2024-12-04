e29397997877dd1d4a5895bd98abf37da9589a7d
navin <ipsitabiswal884@gmail.com>
2024-12-04 16:10:15 +0530

    portfolio

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,131 @@
+
+  <!-- Footer Start -->
+  <footer class="footer-section">
+    <div class="container-footer">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12">
+            <div class="bg-half-100">
+              <div class="row">
+                <div class="col-lg-3 col-md-3">
+                  <div class="colum md-mb50">
+                    <div class="footer-title mb-20">
+                      <h6>Address</h6>
+                    </div>
+
+                    <div class="footer-subtitle footer-text-style">
+                      <p>Germany — 426 Eden Drive Richmond, VA 23219</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-3 offset-lg-1 offset-md-1">
+                  <div class="colum md-mb50">
+                    <div class="footer-title mb-20">
+                      <h6>Say Hello</h6>
+                    </div>
+                    <div class="footer-subtitle footer-text-style">
+                      <p class="mb-10 footer-text-style">
+                        <a href="#0" class="footer-text-style">hello@design.com</a>
+                      </p>
+                      <h5>
+                        <a href="#">+61 214 585 17 69</a>
+                      </h5>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-lg-2 col-md-2 md-mb50">
+                  <div class="footer-title mb-20">
+                    <h6>Social</h6>
+                  </div>
+                  <ul class="social-text">
+                    <li class="mb-2"><a href="#">Facebook</a></li>
+                    <li class="mb-2"><a href="#">Twitter</a></li>
+                    <li class="mb-2"><a href="#">LinkedIn</a></li>
+                    <li class="mb-2"><a href="#">Instagram</a></li>
+                  </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-3">
+                  <div class="footer-title mb-20">
+                    <h6>Newsletter</h6>
+                  </div>
+                  <div class="subscribe">
+                    <form>
+                      <div class="form-group">
+                        <div class="position-relative">
+                          <input type="email" class="form-control" id="emailId" placeholder="Type Your Email">
+                          <button type="submit" class="d-inline-flex align-items-center justify-content-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
+                              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                              stroke-linejoin="round" class="lucide lucide-arrow-right">
+                              <path d="M5 12h14" />
+                              <path d="m12 5 7 7-7 7" />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!--end row-->
+      </div>
+
+      <div class="footer-bar pt-30 pb-30">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="logo">
+                <a href="#"><img src="asset/images/logo-light.png" class="img-fluid"></a>
+              </div>
+            </div><!--end col-->
+
+            <div class="col-lg-6">
+              <div class="copyright d-flex">
+                <div class="ms-auto">
+                  <p class="mb-0">©
+                    <script>document.write(new Date().getFullYear())</script> Natia. Design with <i
+                      class="mdi mdi-heart text-danger"></i> by <a href="https://zoyothemes.com/" target="_blank"
+                      class="text-reset">Zoyothemes</a>.
+                  </p>
+                </div>
+              </div>
+            </div><!--end col-->
+          </div><!--end row-->
+        </div><!--end container-->
+      </div>
+    </div><!--end container-->
+  </footer>
+  <!-- Footer End -->
+
+  <!-- Back to top -->
+  <a href="#" onclick="topFunction()" id="back-to-top" class="back-to-top rounded text-center align-items-center justify-content-center fs-5">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up icons"><polyline points="18 15 12 9 6 15"></polyline></svg>
+  </a>
+  <!-- Back to top -->
+
+  <!-- Bootstrap Js -->
+  <script src="asset/js/bootstrap.bundle.min.js"></script>
+  <script src="asset/js/gumshoe.js"></script>
+  <script src="asset/js/counter.js"></script>
+
+  <!-- Typed Js -->
+  <script src="asset/js/typed.js"></script>
+  <script src="asset/js/scrollCue.min.js"></script>
+
+  <!-- Shuffle Js -->
+  <script src="asset/js/shuffle.min.js"></script>
+  <script src="asset/js/filter.js"></script>
+
+  <!-- swiper -->
+  <script src="asset/js/swiper-bundle.min.js"></script>
+
+  <!-- Main Js -->
+  <script src="asset/js/app.js"></script>
+
+</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Premium Bootstrap 4 Landing Page Template" />
+  <meta name="keywords" content="bootstrap 4, premium, marketing, multipurpose" />
+  <meta name="author" content="Zoyothemes">
+  <title>Natia - Personal Portfolio Template</title>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="images/favicon.ico">
+
+  <!-- Main CSS -->
+  <link href="asset/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link href="asset/css/materialdesignicons.min.css" rel="stylesheet" type="text/css" />
+
+  <!-- Swiper Css -->
+  <link href="asset/css/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
+
+  <!-- Custom styles -->
+  <link href="asset/css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+  <!-- Navbar Start -->
+  <nav class="navbar navbar-expand-lg fixed-top sticky" id="navbar">
+    <div class="container">
+      <a class='navbar-brand' href='index.html'>
+        <span class="logo-light-mode">
+          <img src="asset/images/logo-light.png" class="l-light" alt="">
+          <img src="asset/images/logo-dark.png" class="l-dark" alt="">
+        </span>
+        <img src="asset/images/logo-light.png" class="logo-dark-mode" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0" id="navbar-navlist">
+          <li class="nav-item">
+            <a class="nav-link" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#work">Work</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#testimonial">Testimonial</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#blog">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- End collapse -->
+    </div>
+    <!-- End container -->
+  </nav>
+  <!-- Navbar End -->
